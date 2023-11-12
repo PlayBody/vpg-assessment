a9ce958bbda1e40290e085198e652facea00bc35
AK-47 <playbody2021@gmail.com>
2023-11-13 05:32:03 +0900

    update cache mechanizm

--- .Net_Program.cs
+++ .Net_Program.cs
@@ -22,37 +22,128 @@ namespace TestTemp
 #else
         private static string dataConnectionString = "Data Source=tcp:xxx.database.windows.net;Initial Catalog=mydb;Integrated Security=False;Persist Security Info=False;User ID=xxx;Password=xxxxxx;";
 #endif
-        private static List<int> temperatureCache = new List<int>();
+
+        public class CacheTemperatures
+        {
+            private static int cacheMaxSize = 5;
+            private static int cacheIndex = 0;
+            private static List<CacheTemperatures?> temperatureCacheList = new List<CacheTemperatures?>();
+            private DateTime fromDateTime;
+            private DateTime toDateTime;
+            private List<int> temps;
+            private int index;
+
+            public List<int> Temps
+            {
+                get { return this.temps; }
+            }
+
+            public CacheTemperatures(DateTime fromDateTime, DateTime toDateTime, List<int> temps)
+            {
+                this.Update(fromDateTime, toDateTime, temps);
+            }
+
+            public bool Check(DateTime fromDateTime, DateTime toDateTime)
+            {
+                return this.fromDateTime == fromDateTime && this.toDateTime == toDateTime;
+            }
+
+            public void Update(DateTime fromDateTime, DateTime toDateTime, List<int> temps)
+            {
+                this.temps = temps;
+                this.fromDateTime = fromDateTime;
+                this.toDateTime = toDateTime;
+                this.index = cacheIndex;
+                cacheIndex++;
+            }
+
+            public void UpdateIndex()
+            {
+                this.index = cacheIndex;
+                cacheIndex++;
+            }
+
+            public static void AddTemp(DateTime fromDateTime, DateTime toDateTime, List<int> temps)
+            {
+                int minCachePosition = -1;
+                int minCacheIndex = int.MaxValue;
+                if (temperatureCacheList.Count < cacheMaxSize)
+                {
+                    temperatureCacheList.Add(new CacheTemperatures(fromDateTime, toDateTime, temps));
+                }
+                else
+                {
+                    for (int position = 0; position < temperatureCacheList.Count; position++)
+                    {
+                        var cache = temperatureCacheList[position];
+                        if (cache != null && cache.index < minCacheIndex)
+                        {
+                            minCacheIndex = cache.index;
+                            minCachePosition = position;
+                        }
+                    }
+                    temperatureCacheList[minCachePosition]!.Update(fromDateTime, toDateTime, temps);
+                }
+            }
+
+            public static CacheTemperatures? GetCache(DateTime fromDateTime, DateTime toDateTime)
+            {
+                foreach (var cache in temperatureCacheList)
+                {
+                    if (cache != null && cache.Check(fromDateTime, toDateTime))
+                    {
+                        return cache;
+                    }
+                }
+                return null;
+            }
+
+            public static void Clear(DateTime dateTime)
+            {
+                for (int position = 0; position < temperatureCacheList.Count; position++)
+                {
+                    var cache = temperatureCacheList[position];
+                    if (cache != null && cache.fromDateTime <= dateTime && cache.toDateTime >= dateTime)
+                    {
+                        temperatureCacheList[position] = null;
+                    }
+                }
+            }
+            
+            public static void ClearAll()
+            {
+                cacheIndex = 0;
+                temperatureCacheList.Clear();
+            }
+        }
 
         static List<int> ReadTemperatures(DateTime startDate, DateTime endDate)
         {
-            if (temperatureCache.Count > 0)
-            {
+            CacheTemperatures? cache = CacheTemperatures.GetCache(startDate, endDate);
+            if(cache != null) {
+                cache.UpdateIndex();
                 Console.WriteLine("Used cache for read temperature");
-                return temperatureCache;
+                return cache.Temps;
             }
 
             List<int> temps = new List<int>();
             using (SqlConnection connection = new SqlConnection(dataConnectionString)) {
                 connection.Open();
-                using (SqlCommand command = new SqlCommand("SELECT id, sensor, name, temp, date, guid, remarks FROM TempReadings", connection)){
+                using (SqlCommand command = new SqlCommand($"SELECT id, sensor, name, temp, date, guid, remarks FROM TempReadings WHERE date >= '{startDate.ToString("yyyy-MM-dd HH:mm:ss.fff")}' AND date <= '{endDate.ToString("yyyy-MM-dd HH:mm:ss.fff")}'", connection)){
                     using (SqlDataReader reader = command.ExecuteReader()) {
                         while(reader.Read()) {
-                            DateTime date = (DateTime)reader["date"];
-                            if(date >= startDate && date <= endDate){
-                                int temp = 0;
+                            int temp = 0;
 
-                                if(int.TryParse(reader["temp"].ToString(), out temp)){
-                                    temps.Add(temp);
-                                } else {
-                                    Console.WriteLine("Failed to parse temperature: " + reader["temp"].ToString());
-                                }
+                            if(int.TryParse(reader["temp"].ToString(), out temp)){
+                                temps.Add(temp);
+                            } else {
+                                Console.WriteLine("Failed to parse temperature: " + reader["temp"].ToString());
                             }
                         }
                     }
                 }
             }
-            temperatureCache = temps;
+            CacheTemperatures.AddTemp(startDate, endDate, temps);
             return temps;
         }
 
@@ -70,19 +161,20 @@ namespace TestTemp
 
         static void AddTemperatureReading(int sensor, string name, int temp, Guid guid, string remarks)
         {
+            DateTime now = DateTime.Now;
             using (SqlConnection connection = new SqlConnection(dataConnectionString)){
                 connection.Open();
                 using (SqlCommand command = new SqlCommand("INSERT INTO TempReadings (sensor, name, temp, date, guid, remarks) VALUES (@sensor, @name, @temp, @date, @guid, @remarks)", connection)){
                     command.Parameters.AddWithValue("@sensor", sensor);
                     command.Parameters.AddWithValue("@name", name);
                     command.Parameters.AddWithValue("@temp", temp);
-                    command.Parameters.AddWithValue("@date", DateTime.Now);
+                    command.Parameters.AddWithValue("@date", now);
                     command.Parameters.AddWithValue("@guid", guid);
                     command.Parameters.AddWithValue("@remarks", remarks);
                     command.ExecuteNonQuery();
                 }
             }
-            temperatureCache.Clear();
+            CacheTemperatures.Clear(now);
         }
 
         static void Main(string[] args)
--- Python_Program.py
+++ Python_Program.py
@@ -7,10 +7,55 @@ import random
 import mysql.connector
 
 # switch mysql/psycopg2
-use_mysql = False
+# use_mysql = False
+use_mysql = True
 
-# This value will be used for caching
-temperature_cache_list = []
+class CacheTemperatures:
+    cache_max_size = 5
+    cache_index = 0
+    temperature_cache_list = []
+    def __init__(self, from_datetime: datetime, to_datetime: datetime, temps) -> None:
+        self.update(from_datetime, to_datetime, temps)
+    def check(self, from_datetime: datetime, to_datetime: datetime):
+        if self.from_date == from_datetime and self.to_date == to_datetime:
+            return True
+        return False
+    def update(self, from_datetime: datetime, to_datetime: datetime, temps):
+        self.temps = temps
+        self.from_date = from_datetime
+        self.to_date = to_datetime
+        self.index = CacheTemperatures.cache_index
+        CacheTemperatures.cache_index += 1
+    def update_index(self):
+        self.index = CacheTemperatures.cache_index
+        CacheTemperatures.cache_index += 1
+    @classmethod
+    def add_temp(cls, from_datetime: datetime, to_datetime: datetime, temps):
+        min_cache_position = -1
+        min_cache_index = 1e9
+        if len(cls.temperature_cache_list) < cls.cache_max_size:
+            cls.temperature_cache_list.append(CacheTemperatures(from_datetime, to_datetime, temps))
+        else:
+            for (postion, cache) in enumerate(cls.temperature_cache_list):
+                if cache is not None and cache.cache_index < min_cache_index:
+                    min_cache_index = cache.cache_index
+                    min_cache_position = postion
+            cls.temperature_cache_list[min_cache_position].update(from_datetime, to_datetime, temps)
+    @classmethod
+    def get_cache(cls, from_datetime: datetime, to_datetime: datetime):
+        for cache in cls.temperature_cache_list:
+            if cache is not None and cache.check(from_datetime, to_datetime):
+                return cache
+        return None
+    @classmethod
+    def clear(cls, dt:datetime):
+        for (postion, cache) in enumerate(cls.temperature_cache_list):
+            if cache is not None and cache.from_date <= dt and cache.to_date >= dt:
+                cls.temperature_cache_list[postion] = None
+    @classmethod
+    def clear_all(cls):
+        cls.cache_index = 0
+        cls.temperature_cache_list = []
 
 def ConnectionDB():
     global use_mysql
@@ -22,28 +67,28 @@ def ConnectionDB():
     return connection, connection.cursor()
 
 def ReadTemperatures(from_datetime: datetime, to_datetime: datetime):
-    global temperature_cache_list
-    if temperature_cache_list:
+    cache = CacheTemperatures.get_cache(from_datetime, to_datetime)
+    if cache is not None:
+        cache.update_index()
         print("Used cache for read temperature")
-        return temperature_cache_list
+        return cache.temps
     temperature_list = []
     con, cur = ConnectionDB()
-    sql = "SELECT id, sensor, name, temp, date, guid, remarks FROM TempReadings"
+    sql = f"SELECT id, sensor, name, temp, date, guid, remarks FROM TempReadings WHERE date >= '{str(from_datetime)}' AND date <= '{str(to_datetime)}'"
     cur.execute(sql)
     # id = 0, sensor = 1, name = 2, temp = 3, date = 4, guid = 5, remarks = 6
     while True:
         data = cur.fetchone()
         if data is None:
             break
-        if data[4] <= to_datetime and data[4] >= from_datetime:
-            temp = 0
-            try:
-                temp = int(data[3])
-                temperature_list.append(temp)
-            except:
-                print("Failed to parse temperature:", data[3])
+        temp = 0
+        try:
+            temp = int(data[3])
+            temperature_list.append(temp)
+        except:
+            print("Failed to parse temperature:", data[3])
     con.close()
-    temperature_cache_list = temperature_list
+    CacheTemperatures.add_temp(from_datetime, to_datetime, temperature_list)
     return temperature_list
 
 def CalculateStatistics(from_datetime: datetime, to_datetime: datetime):
@@ -58,14 +103,13 @@ def CalculateStatistics(from_datetime: datetime, to_datetime: datetime):
     return [mean, median, min_temp, max_temp, std_dev]
 
 def AddTemperatureReading(sensor: int, name: str, temp: int, guid: uuid.UUID, remarks: str):
-    global temperature_cache_list
     con, cur = ConnectionDB()
+    now = datetime.now()
     sql = "INSERT INTO TempReadings (sensor, name, temp, date, guid, remarks) VALUES (%s, %s, %s, %s, %s, %s)"
     cur.execute(sql, (sensor, name, temp, datetime.now(), str(guid), remarks))
     con.commit()
     con.close()
-    temperature_cache_list = []
-
+    CacheTemperatures.clear(now)
 
 # some test cases
 start = datetime.now() - timedelta(days=1)
